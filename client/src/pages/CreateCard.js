264c2a3e287de1512dff4e969637289f8099fe77
Syd Shields <sshields750@gmail.com>
2022-01-22 18:39:29 -0500

    create new card page

---
+++ client/src/pages/CreateCard.js
@@ -0,0 +1,43 @@
+import { useNavigate } from "react-router";
+import Nav from "../compontents/Nav";
+import './styles/pages.css'
+
+function CreateCard() {
+    const navigate = useNavigate();
+
+    return(
+        <div className='page'>
+            <Nav />
+        <div className='create-card'>
+            <h2>Create Card</h2>
+            <form>
+            <label>Your Song:</label>
+            <button onClick={((e) => {
+                e.preventDefault()
+            })}>Pick a Song</button>
+            <label>Bio:</label>
+            <textarea></textarea>
+            <label>Emoticon:</label>
+            <div className='emoticons'></div>
+            <label>Instagram:</label>
+            <input type='text'></input>
+            <label>YouTube:</label>
+            <input type='text'></input>
+            <label>Twitter:</label>
+            <input type='text'></input>
+            <label>TikTok:</label>
+            <input type='text'></input>
+            <label>Spotify:</label>
+            <input type='text'></input>
+            <label>Apple Music:</label>
+            <input type='text'></input>
+            <label>Card Color:</label>
+            <input className='colour-selector' type='color'></input>
+            <input type='submit' value='Create Card'></input>
+            </form>
+        </div>
+        </div>
+    )
+}
+
+export default CreateCard;
